a04b8f7e7135587230e1cdf37f57362ba2626249
Aymen Hammami <hammami.aym@outlook.com>
2022-05-23 00:03:41 +0100

    Added typewriter

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,20 @@ import React from "react";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 import bgParticles from "../assets/bgParticles.json";
+import TypeWriter from "typewriter-effect";
 
 function Home() {
   const particlesInit = async (main) => {
     await loadFull(main);
   };
+
+  const messages = [
+    "Passionate, Creative, Quick Learner, Problem Solver.",
+    "I always seek to sharpen & improve my skills.",
+    "I want to excel in leadership, teamwork and development.",
+    "I want to learn new technologies and apply them.",
+    "I want to work as a game developer one day.",
+  ];
 
   return (
     <>
@@ -25,11 +34,32 @@ function Home() {
               <Typography variant="h2" fontWeight="bold">
                 Hey there!
               </Typography>
-              <Typography variant="h3">Welcome to my Website!</Typography>
+              <Typography variant="h4">Welcome to my Website!</Typography>
             </div>
             <Typography variant="h2" fontWeight="bold">
               I'm <span style={{ color: "aqua" }}>Aymen Hammami</span>
             </Typography>
+            <TypeWriter
+              onInit={(typewriter) => {
+                typewriter.pauseFor(2000);
+                messages.forEach((element) => {
+                  typewriter
+                    .typeString(element)
+                    .pauseFor(750)
+                    .deleteAll()
+                    .pauseFor(250);
+                });
+                typewriter.start();
+              }}
+              options={{
+                wrapperClassName: "typewriter",
+                cursorClassName: "Typewriter__cursor typewriter-cursor",
+                autoStart: false,
+                loop: true,
+                delay: 75,
+                deleteSpeed: 10,
+              }}
+            />
           </div>
           <div className="intro-picture"></div>
         </div>
