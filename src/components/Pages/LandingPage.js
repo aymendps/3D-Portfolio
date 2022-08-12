0575efeea15d64e6fe578bfb3cdbc4872ab694c5
Aymen Hammami <hammami.aym@outlook.com>
2022-08-12 02:02:33 +0100

    set to new version

--- src/components/Pages/LandingPage.js
+++ src/components/Pages/LandingPage.js
@@ -13,79 +13,98 @@ function LandingPage({ setIsClicked }) {
   };
 
   return (
-    <AnimatePresence>
-      {!shouldExit && (
-        <motion.div
-          exit={{ opacity: 0, scale: 0 }}
-          transition={{ duration: 0.5 }}
-          className="fixed w-full h-[100vh] top-0 left-0 z-10"
-        >
-          <motion.h2
-            initial={{
-              textAlign: "center",
-              verticalAlign: "middle",
-              lineHeight: "100vh",
-            }}
-            animate={{ rotateX: [0, 90], display: "none" }}
-            transition={{
-              rotateX: { delay: 1, duration: 0.5 },
-              display: { delay: 1.5 },
-            }}
-            className="font-berkshire bold text-[3.75rem] font-bold"
+    <>
+      <AnimatePresence>
+        {!shouldExit && (
+          <motion.div
+            exit={{ opacity: 0, scale: 0 }}
+            transition={{ duration: 0.5 }}
+            className="fixed w-full h-[100vh] top-0 left-0 z-10"
           >
-            Hey there!
-          </motion.h2>
-          <motion.h2
-            initial={{
-              textAlign: "center",
-              verticalAlign: "middle",
-              lineHeight: "100vh",
-            }}
-            animate={{ rotateX: [90, 0, 0, 90], display: "none" }}
-            transition={{
-              rotateX: { delay: 2, times: [0, 0.3, 0.7, 1], duration: 3 },
-              display: { delay: 5 },
-            }}
-            className="font-berkshire bold text-[3.75rem] font-bold"
-          >
-            Thanks for checking out my website!
-          </motion.h2>
-          <div className="flex flex-col items-center justify-center w-full h-full gap-8">
             <motion.h2
               initial={{
                 textAlign: "center",
                 verticalAlign: "middle",
+                lineHeight: "100vh",
               }}
-              animate={{ rotateX: [90, 0] }}
+              animate={{ rotateX: [0, 90], display: "none" }}
               transition={{
-                rotateX: { delay: 5.5, duration: 0.5 },
+                rotateX: { delay: 1, duration: 0.5 },
+                display: { delay: 1.5 },
               }}
-              className="font-berkshire text-[3.75rem] font-bold"
+              className="font-berkshire bold text-[3.75rem] font-bold"
             >
-              Are you ready?
+              Hey there!
             </motion.h2>
-            <motion.div
-              initial={{ opacity: 0, pointerEvents: "none" }}
-              animate={{ opacity: 1, pointerEvents: "all" }}
-              transition={{ delay: 5.8, duration: 0.3 }}
-              className="flex flex-col items-center justify-center gap-4"
+            <motion.h2
+              initial={{
+                textAlign: "center",
+                verticalAlign: "middle",
+                lineHeight: "100vh",
+              }}
+              animate={{ rotateX: [90, 0, 0, 90], display: "none" }}
+              transition={{
+                rotateX: { delay: 2, times: [0, 0.3, 0.7, 1], duration: 3 },
+                display: { delay: 5 },
+              }}
+              className="font-berkshire bold text-[3.75rem] font-bold"
             >
-              <Button
-                onClick={handleClick}
-                variant="outlined"
-                sx={{ transition: "250ms !important" }}
-                className="font-berkshire text-[2.2rem] hover:text-[2.5rem] font-bold normal-case px-12 rounded-3xl text-cyan-300 border-cyan-300"
+              Thanks for checking out my website!
+            </motion.h2>
+            <div className="flex flex-col items-center justify-center w-full h-full gap-8">
+              <motion.h2
+                initial={{
+                  textAlign: "center",
+                  verticalAlign: "middle",
+                }}
+                animate={{ rotateX: [90, 0] }}
+                transition={{
+                  rotateX: { delay: 5.5, duration: 0.5 },
+                }}
+                className="font-berkshire text-[3.75rem] font-bold"
               >
-                {"> Let's go <"}
-              </Button>
-              <Typography className="font-berkshire text-[1.5rem] text-cyan-400">
-                For a better experience, please enable fullscreen mode 'F11'
-              </Typography>
-            </motion.div>
-          </div>
-        </motion.div>
-      )}
-    </AnimatePresence>
+                Are you ready?
+              </motion.h2>
+              <motion.div
+                initial={{ opacity: 0, pointerEvents: "none" }}
+                animate={{ opacity: 1, pointerEvents: "all" }}
+                transition={{ delay: 5.8, duration: 0.3 }}
+                className="flex flex-col items-center justify-center gap-4"
+              >
+                <Button
+                  onClick={handleClick}
+                  variant="outlined"
+                  sx={{ transition: "250ms !important" }}
+                  className="font-berkshire text-[2.2rem] hover:text-[2.5rem] font-bold normal-case px-12 rounded-3xl text-cyan-300 border-cyan-300"
+                >
+                  {"> Let's go <"}
+                </Button>
+                <Typography className="font-berkshire text-[1.5rem] text-cyan-400">
+                  For a better experience, please enable fullscreen mode 'F11'
+                </Typography>
+              </motion.div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <AnimatePresence>
+        {!shouldExit && (
+          <motion.div
+            initial={{ x: "-100%", y: "10%" }}
+            animate={{ x: "5%", y: "10%" }}
+            exit={{ x: "-100%", y: "10%" }}
+            transition={{ duration: 0.5 }}
+            className="absolute z-20"
+          >
+            <Typography className="font-berkshire">
+              Version: 0.5.0
+              <br />
+              August 2022
+            </Typography>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
   );
 }
 
