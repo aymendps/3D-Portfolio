8c232827560bd7199c13dbd07ade0a8a77f8a9aa
Aymen Hammami <hammami.aym@outlook.com>
2022-08-31 18:24:36 +0100

    added assets and tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
     extend: {
       colors: {
         "my-brown": "rgb(63,44,35)",
+        "my-light-brown": "#8C624F",
+        "my-orange-brown": "#A66E1B",
       },
       fontFamily: {
         berkshire: ['"Berkshire Swash"'],
@@ -15,6 +17,8 @@ module.exports = {
       backgroundImage: {
         "paper-image":
           "url('../public/assets/models/office/textures/old_paper_baseColor.png')",
+        "orange-sticker": "url('../public/assets/images/orange-sticker.png')",
+        "yellow-sticker": "url('../public/assets/images/yellow-sticker.png')",
       },
       backgroundSize: {
         "size-25%": "25%",
