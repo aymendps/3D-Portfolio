ed35e2734950bac06b49a38a410ef6f9bd0f48ff
Aymen Hammami <hammami.aym@outlook.com>
2022-09-02 02:36:53 +0100

    more customization to DecoratedTitlen, added CoverPage to use with all papers

---
+++ src/components/Pages/Me/Shared/CoverPage.js
@@ -0,0 +1,53 @@
+import { Typography } from "@mui/material";
+import DecoratedTitle from "./DecoratedTitle";
+
+function CoverPage({ title }) {
+  return (
+    <div className="w-full h-full flex flex-col justify-evenly items-center">
+      <DecoratedTitle title={title} variant="h1" decorationSize={150} />
+      <div className="w-full h-[40%]">
+        <Typography
+          variant="h4"
+          className="font-berkshire text-my-light-brown text-center"
+        >
+          To navigate the papers, use:
+        </Typography>
+        <div className="flex w-full h-full">
+          <div className="w-1/2 flex flex-col justify-center items-center">
+            <img
+              className="w-[23%]"
+              alt=""
+              src="/assets/images/mouse.svg"
+            ></img>
+            <Typography variant="h6" className="font-berkshire text-my-brown">
+              Mouse Wheel
+            </Typography>
+          </div>
+          <div className="w-1/2 flex flex-col justify-center items-center gap-2">
+            <div className="w-[10%] aspect-square border-8 border-my-orange-brown flex items-center justify-center">
+              <Typography
+                variant="h5"
+                className="font-berkshire text-my-orange-brown"
+              >
+                W
+              </Typography>
+            </div>
+            <div className="w-[10%] aspect-square border-8 border-my-orange-brown flex items-center justify-center">
+              <Typography
+                variant="h5"
+                className="font-berkshire text-my-orange-brown"
+              >
+                S
+              </Typography>
+            </div>
+            <Typography variant="h6" className="font-berkshire text-my-brown">
+              Keyboard
+            </Typography>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CoverPage;
--- src/components/Pages/Me/Shared/DecoratedTitle.js
+++ src/components/Pages/Me/Shared/DecoratedTitle.js
@@ -1,22 +1,22 @@
 import { Typography } from "@mui/material";
 
-function DecoratedTitle({ title }) {
+function DecoratedTitle({ title, variant, decorationSize }) {
   return (
     <div className="w-full flex items-center justify-center gap-4">
       <img
-        className="w-[120px]"
+        className={`w-[${decorationSize}px]`}
         style={{ transform: "rotateY(180deg)" }}
         src="/assets/images/title-decoration.png"
         alt=""
       ></img>
       <Typography
         className="font-berkshire text-center text-my-brown font-bold"
-        variant="h2"
+        variant={variant}
       >
         {title}
       </Typography>
       <img
-        className="w-[120px]"
+        className={`w-[${decorationSize}px]`}
         src="/assets/images/title-decoration.png"
         alt=""
       ></img>
