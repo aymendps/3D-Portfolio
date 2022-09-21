640be0c7bd60503ca850988132551da1651c6d4f
Aymen Hammami <hammami.aym@outlook.com>
2022-09-21 19:14:49 +0100

    improvements to project css

--- src/components/Three Fiber/UI/UI pages/Project.js
+++ src/components/Three Fiber/UI/UI pages/Project.js
@@ -76,8 +76,8 @@ function Project({
       </div>
     </PinnedDiv>
   ) : (
-    <div className="flex flex-col w-full h-full justify-evenly px-4">
-      <div className="flex items-center gap-8 w-full">
+    <div className="flex flex-col w-full h-full px-4">
+      <div className="flex items-center gap-8 w-full basis-[15%]">
         <IconButton
           size="small"
           disableRipple
@@ -86,17 +86,20 @@ function Project({
         >
           <ArrowBackIcon className="text-my-brown text-4xl" />
         </IconButton>
-        <Typography variant="h4" className="font-berkshire text-my-brown">
+        <Typography
+          variant="h4"
+          className="font-berkshire text-my-brown text-[2rem]"
+        >
           {title}
         </Typography>
         {[...mainTech, ...otherTech].map((tech, index) => {
           return <TechIcon key={index + "tech" + tech} url={tech} />;
         })}
       </div>
-      <div className="w-full flex items-center">
-        <div className="w-full flex">
-          <div className="basis-3/4 2xl:basis-2/3 flex flex-col ">
-            <div className="w-full flex flex-col gap-[5%]">
+      <div className="w-full flex basis-[85%]">
+        <div className="w-full flex items-center">
+          <div className="basis-3/4 2xl:basis-2/3 flex flex-col justify-center">
+            <div className="w-full flex flex-col gap-[5%] justify-center">
               <div className="w-[95%]">
                 {selectedMedia === 0 ? (
                   <VideoPlayer
@@ -120,18 +123,18 @@ function Project({
               <div className="w-[95%] flex justify-between">{swapMedia}</div>
             </div>
           </div>
-          <div className="basis-1/4 2xl:basis-1/3 flex flex-col gap-[10%]">
-            <Typography className="font-berkshire text-my-brown whitespace-pre-line text-[1.05rem] 2xl:text-[1.1rem]">
+          <div className="basis-1/4 2xl:basis-1/3 flex flex-col h-[94%] 2xl:h-full">
+            <Typography className="font-berkshire basis-[82%] flex-shrink-0 overflow-hidden text-my-brown whitespace-pre-line text-[0.94rem] 2xl:text-[1.1rem]">
               {description}
             </Typography>
-            <div>
+            <div className="basis-[18%] flex flex-col justify-center gap-2">
               {github && (
                 <Button
                   fullWidth
                   startIcon={<GithubIcon />}
                   disableRipple
                   variant="contained"
-                  className="normal-case font-berkshire bg-my-orange-brown mb-2"
+                  className="normal-case font-berkshire bg-my-orange-brown hover:bg-red-700 text-[0.75rem] 2xl:text-[0.875rem]"
                   onClick={() => {
                     window.open(github, "_blank");
                   }}
@@ -139,34 +142,34 @@ function Project({
                   Open Repository
                 </Button>
               )}
-              {download && (
-                <Button
-                  fullWidth
-                  startIcon={<DownloadIcon />}
-                  disableRipple
-                  variant="contained"
-                  className="normal-case font-berkshire bg-my-orange-brown mt-2 mb-2"
-                  onClick={() => {
-                    window.open(download, "_blank");
-                  }}
-                >
-                  Download Demo
-                </Button>
-              )}
-              {moreScreenshots && (
-                <Button
-                  fullWidth
-                  startIcon={<CollectionsIcon />}
-                  disableRipple
-                  variant="contained"
-                  className="normal-case font-berkshire bg-my-orange-brown mt-2"
-                  onClick={() => {
-                    window.open(moreScreenshots, "_blank");
-                  }}
-                >
-                  More Screenshots
-                </Button>
-              )}
+              <div className="flex flex-col 2xl:flex-row justify-between w-full gap-2">
+                {download && (
+                  <Button
+                    startIcon={<DownloadIcon />}
+                    disableRipple
+                    variant="contained"
+                    className="normal-case font-berkshire bg-my-orange-brown basis-[48%] flex-grow hover:bg-red-700 text-[0.75rem] 2xl:text-[0.875rem]"
+                    onClick={() => {
+                      window.open(download, "_blank");
+                    }}
+                  >
+                    Download Demo
+                  </Button>
+                )}
+                {moreScreenshots && (
+                  <Button
+                    startIcon={<CollectionsIcon />}
+                    disableRipple
+                    variant="contained"
+                    className="normal-case font-berkshire bg-my-orange-brown basis-[48%] flex-grow hover:bg-red-700 text-[0.75rem] 2xl:text-[0.875rem]"
+                    onClick={() => {
+                      window.open(moreScreenshots, "_blank");
+                    }}
+                  >
+                    More Screenshots
+                  </Button>
+                )}
+              </div>
             </div>
           </div>
         </div>
